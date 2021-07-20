b12c1bccf8a4f5d16b07d392f350ea31f31531ec
Jefferson Didi Silva <didi.silva.dev@gmail.com>
2021-07-20 16:18:30 -0300

    Add Operation Result

---
+++ src/SharedKernel/Orion.OperationResult/Implementations/OperationResult.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Orion.OperationResult.Implementations
+{
+    public sealed class OperationResult<TResult> : 
+        Result<TResult, List<string>>
+    {
+        private OperationResult(
+            bool success,
+            TResult value,
+            List<string> errors
+        ) : base(success, value, errors) {}
+
+        private static OperationResult<TResult> Fail(List<string> errors) => 
+            new(false, default, errors);
+
+        private static OperationResult<TResult> Ok(TResult value) =>
+            new(true, value, default);
+
+        public static implicit operator OperationResult<TResult>(List<string> errors) => 
+            Fail(errors);
+        
+        public static implicit operator OperationResult<TResult>(string error) => 
+            Fail(new List<string> { error });
+
+        public static implicit operator OperationResult<TResult>(TResult value) =>
+            Ok(value);
+    }
+}
