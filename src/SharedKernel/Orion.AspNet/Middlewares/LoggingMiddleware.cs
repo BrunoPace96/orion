3812877844a64f44f8b02d4ee1016f14a3fa52d3
Jefferson Didi Silva <didi.silva.dev@gmail.com>
2021-07-20 18:00:03 -0300

    Add ASPNET

---
+++ src/SharedKernel/Orion.AspNet/Middlewares/LoggingMiddleware.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Orion.AspNet.Middlewares.Extensions;
+
+namespace Orion.AspNet.Middlewares
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly Action<string> _log;
+
+        public LoggingMiddleware(RequestDelegate next, Action<string> log)
+        {
+            _next = next;
+            _log = log;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var request = await context.GetRequestPresentationAsync();
+            _log.Invoke(request);
+            
+            Stream SaveCopyOfOriginalBodyStream(out MemoryStream memoryStream)
+            {
+                var stream = context.Response.Body;
+                memoryStream = new MemoryStream();
+                context.Response.Body = memoryStream;
+                return stream;
+            }
+            
+            var originalBodyStream = SaveCopyOfOriginalBodyStream(out var responseBody);
+
+            await _next(context);
+
+            var response = await context.GetResponsePresentationAsync();
+            _log.Invoke(response);
+
+            await responseBody.CopyToAsync(originalBodyStream);
+        }
+    }
+}
