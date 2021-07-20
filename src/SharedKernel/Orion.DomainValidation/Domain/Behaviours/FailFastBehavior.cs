aefbcdc1a86b56ae7c289d60657b37329c2209e2
Jefferson Didi Silva <didi.silva.dev@gmail.com>
2021-07-20 17:15:32 -0300

    Add Domain Validation

---
+++ src/SharedKernel/Orion.DomainValidation/Domain/Behaviours/FailFastBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using MediatR;
+using Orion.DomainValidation.DataContracts;
+
+namespace Orion.DomainValidation.Domain.Behaviours
+{
+    public class FailFastBehavior<TRequest, TResponse> :
+        IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+        private readonly IDomainValidationProvider _domainValidationProvider;
+
+        public FailFastBehavior(
+            IEnumerable<IValidator<TRequest>> validators,
+            IDomainValidationProvider domainValidationProvider
+        )
+        {
+            _validators = validators;
+            _domainValidationProvider = domainValidationProvider;
+        }
+
+        public async Task<TResponse> Handle(
+            TRequest request,
+            CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next
+        )
+        {
+            var errors = _validators
+                .Select(e => e.Validate(request))
+                .SelectMany(e => e.Errors)
+                .Select(e => new DomainValidationNotification(e.ErrorMessage, e.PropertyName))
+                .ToList();
+
+            if (errors.Any())
+            {
+                _domainValidationProvider.AddValidationErrors(errors);
+                return default;
+            }
+
+            return await next();
+        }
+    }
+}
---
+++ src/SharedKernel/Orion.DomainValidation/Domain/IDomainValidationProvider.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+using Orion.DomainValidation.DataContracts;
+
+namespace Orion.DomainValidation.Domain
+{
+    public interface IDomainValidationProvider
+    {
+        bool HasErrors();
+        bool HasNoErrors();
+        List<DomainValidationNotification> GetErrors();
+        void AddValidationError(string message, string field = "default");
+        void AddValidationError(DomainValidationNotification validationNotification);
+        void AddValidationErrors(IEnumerable<DomainValidationNotification> validations);
+        void AddUnauthorizedError(string message = "Unauthorized!");
+        void AddForbiddenError(string message = "Forbidden!");
+        void AddNotFoundError(string message = "Not Found!");
+    }
+}
