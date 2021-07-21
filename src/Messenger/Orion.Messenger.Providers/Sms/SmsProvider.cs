e5b47e061311577636b2b1dcba6187506881f602
Jefferson Didi Silva <didi.silva.dev@gmail.com>
2021-07-20 23:41:02 -0300

    Add Messenger Grpc Sms Service

---
+++ src/Messenger/Orion.Messenger.Providers/Sms/SmsProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Orion.Messenger.Providers.Sms
+{
+    public class SmsProvider : ISmsProvider
+    {
+        public Task SendAsync(string phone, string content)
+        {
+            Console.WriteLine($"Sending sms to {phone}");
+            Console.WriteLine(content);
+            return Task.CompletedTask;
+        }
+    }
+}
